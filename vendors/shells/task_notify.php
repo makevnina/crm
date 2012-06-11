3a3e7a31c64c768f9df176af38a3e91eead51d0e
Mike Kolganov <mike.kolganov@gmail.com>
2012-06-11 23:50:28 +0700

    Getting time on PHP

--- vendors/shells/task_notify.php
+++ vendors/shells/task_notify.php
@@ -13,14 +13,16 @@ class TaskNotifyShell extends Shell {
 
 		$this->users = array();
 
-		foreach ($this->_getExpiredSoon() as $task) {
+		$serverTime = time();
+
+		foreach ($this->_getExpiredSoon($serverTime) as $task) {
 			$user = $task['User'];
 			if (empty($user['email'])) continue;
 			if (empty($this->users[ $user['id'] ])) $this->users[ $user['id'] ] = $user;
 			$this->users[ $user['id'] ]['expired_soon'][] = $task['Task'];
 		}
 
-		foreach ($this->_getAlreadyExpired() as $task) {
+		foreach ($this->_getAlreadyExpired($serverTime) as $task) {
 			$user = $task['User'];
 			if (empty($user['email'])) continue;
 			if (empty($this->users[ $user['id'] ])) $this->users[ $user['id'] ] = $user;
@@ -40,26 +42,26 @@ class TaskNotifyShell extends Shell {
 		}
 	}
 
-	private function _getExpiredSoon() {
+	private function _getExpiredSoon($serverTime) {
 		return $this->Task->query("
 			SELECT *
 			FROM tasks as `Task`
 			LEFT JOIN users AS `User` ON `User`.`id` = `Task`.`user_id`
 			WHERE
-				`Task`.`deadline` <= DATE_ADD(NOW(), INTERVAL 30 MINUTE)
+				`Task`.`deadline` <= DATE_ADD(FROM_UNIXTIME({$serverTime}), INTERVAL 30 MINUTE)
 				AND
-				`Task`.`deadline` > NOW()
+				`Task`.`deadline` > FROM_UNIXTIME({$serverTime})
 				AND
 				`Task`.`task_status_id` <> 1
 		");
 	}
-	private function _getAlreadyExpired() {
+	private function _getAlreadyExpired($serverTime) {
 		return $this->Task->query("
 			SELECT *
 			FROM tasks as `Task`
 			LEFT JOIN users AS `User` ON `User`.`id` = `Task`.`user_id`
 			WHERE
-				`Task`.`deadline` < NOW()
+				`Task`.`deadline` < FROM_UNIXTIME({$serverTime})
 				AND
 				`Task`.`task_status_id` <> 1
 		");
