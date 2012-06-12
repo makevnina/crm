9dc96d3046c86884d874c249872f8a1a6447f767
Mike Kolganov <mike.kolganov@gmail.com>
2012-06-12 23:18:02 +0700

    Schema changed

--- crm_mysql_schema.sql
+++ crm_mysql_schema.sql
@@ -1,37 +1,12 @@
--- phpMyAdmin SQL Dump
--- version 3.4.10.1deb1
--- http://www.phpmyadmin.net
---
--- Хост: localhost
--- Время создания: Июн 11 2012 г., 23:26
--- Версия сервера: 5.5.22
--- Версия PHP: 5.3.10-1ubuntu3.1
-
 SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
-
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
 /*!40101 SET NAMES utf8 */;
 
---
--- База данных: `polly_crm`
---
 
--- Dumping database structure for crm
-DROP DATABASE IF EXISTS `crm`;
-CREATE DATABASE IF NOT EXISTS `crm` /*!40100 DEFAULT CHARACTER SET utf8 */;
-USE `crm`;
-
--- --------------------------------------------------------
-
---
--- Структура таблицы `clients`
---
-
-DROP TABLE IF EXISTS `clients`;
 CREATE TABLE IF NOT EXISTS `clients` (
   `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
   `company_id` int(10) unsigned NOT NULL,
@@ -45,13 +20,6 @@ CREATE TABLE IF NOT EXISTS `clients` (
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 AUTO_INCREMENT=1 ;
 
--- --------------------------------------------------------
-
---
--- Структура таблицы `client_statuses`
---
-
-DROP TABLE IF EXISTS `client_statuses`;
 CREATE TABLE IF NOT EXISTS `client_statuses` (
   `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
   `name` varchar(100) NOT NULL,
@@ -60,13 +28,6 @@ CREATE TABLE IF NOT EXISTS `client_statuses` (
   UNIQUE KEY `name` (`name`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
 
--- --------------------------------------------------------
-
---
--- Структура таблицы `comments`
---
-
-DROP TABLE IF EXISTS `comments`;
 CREATE TABLE IF NOT EXISTS `comments` (
   `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
   `user_id` int(10) unsigned NOT NULL,
@@ -78,13 +39,6 @@ CREATE TABLE IF NOT EXISTS `comments` (
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
 
--- --------------------------------------------------------
-
---
--- Структура таблицы `companies`
---
-
-DROP TABLE IF EXISTS `companies`;
 CREATE TABLE IF NOT EXISTS `companies` (
   `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
   `state_id` int(10) unsigned NOT NULL,
@@ -95,13 +49,6 @@ CREATE TABLE IF NOT EXISTS `companies` (
   UNIQUE KEY `name` (`name`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 AUTO_INCREMENT=1 ;
 
--- --------------------------------------------------------
-
---
--- Структура таблицы `completed_projects`
---
-
-DROP TABLE IF EXISTS `completed_projects`;
 CREATE TABLE IF NOT EXISTS `completed_projects` (
   `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
   `project_id` int(10) unsigned NOT NULL DEFAULT '0',
@@ -109,13 +56,6 @@ CREATE TABLE IF NOT EXISTS `completed_projects` (
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
 
--- --------------------------------------------------------
-
---
--- Структура таблицы `emails`
---
-
-DROP TABLE IF EXISTS `emails`;
 CREATE TABLE IF NOT EXISTS `emails` (
   `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
   `artifact_id` int(10) unsigned NOT NULL,
@@ -124,13 +64,6 @@ CREATE TABLE IF NOT EXISTS `emails` (
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
 
--- --------------------------------------------------------
-
---
--- Структура таблицы `phones`
---
-
-DROP TABLE IF EXISTS `phones`;
 CREATE TABLE IF NOT EXISTS `phones` (
   `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
   `artifact_id` int(10) unsigned NOT NULL,
@@ -139,13 +72,6 @@ CREATE TABLE IF NOT EXISTS `phones` (
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
 
--- --------------------------------------------------------
-
---
--- Структура таблицы `projects`
---
-
-DROP TABLE IF EXISTS `projects`;
 CREATE TABLE IF NOT EXISTS `projects` (
   `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
   `name` varchar(100) NOT NULL,
@@ -162,13 +88,6 @@ CREATE TABLE IF NOT EXISTS `projects` (
   UNIQUE KEY `name` (`name`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
 
--- --------------------------------------------------------
-
---
--- Структура таблицы `project_statuses`
---
-
-DROP TABLE IF EXISTS `project_statuses`;
 CREATE TABLE IF NOT EXISTS `project_statuses` (
   `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
   `name` varchar(100) NOT NULL,
@@ -178,13 +97,6 @@ CREATE TABLE IF NOT EXISTS `project_statuses` (
   UNIQUE KEY `name` (`name`)
 ) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=3 ;
 
--- --------------------------------------------------------
-
---
--- Структура таблицы `states`
---
-
-DROP TABLE IF EXISTS `states`;
 CREATE TABLE IF NOT EXISTS `states` (
   `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
   `name` varchar(100) NOT NULL,
@@ -193,13 +105,6 @@ CREATE TABLE IF NOT EXISTS `states` (
   UNIQUE KEY `name` (`name`)
 ) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=4 ;
 
--- --------------------------------------------------------
-
---
--- Структура таблицы `tasks`
---
-
-DROP TABLE IF EXISTS `tasks`;
 CREATE TABLE IF NOT EXISTS `tasks` (
   `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
   `name` varchar(100) NOT NULL,
@@ -209,19 +114,13 @@ CREATE TABLE IF NOT EXISTS `tasks` (
   `client_id` int(10) unsigned NOT NULL DEFAULT '0',
   `project_id` int(10) unsigned NOT NULL DEFAULT '0',
   `type` enum('звонок','встреча','письмо','событие') NOT NULL,
+  `notified` tinyint(1) NOT NULL DEFAULT '0',
   `description` varchar(500) NOT NULL,
   `deadline` datetime NOT NULL,
   PRIMARY KEY (`id`),
   UNIQUE KEY `name` (`name`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=4 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=5 ;
 
--- --------------------------------------------------------
-
---
--- Структура таблицы `task_statuses`
---
-
-DROP TABLE IF EXISTS `task_statuses`;
 CREATE TABLE IF NOT EXISTS `task_statuses` (
   `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
   `name` varchar(100) NOT NULL,
@@ -230,13 +129,6 @@ CREATE TABLE IF NOT EXISTS `task_statuses` (
   UNIQUE KEY `name` (`name`)
 ) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=3 ;
 
--- --------------------------------------------------------
-
---
--- Структура таблицы `users`
---
-
-DROP TABLE IF EXISTS `users`;
 CREATE TABLE IF NOT EXISTS `users` (
   `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
   `login` varchar(50) NOT NULL,
